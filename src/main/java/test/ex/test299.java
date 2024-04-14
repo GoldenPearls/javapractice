28751baa914335e8c31853dbbdf5f8c2a33f163b
Geυмjυ lee <97003348+GoldenPearls@users.noreply.github.com>
2024-04-14 12:37:38 +0900

    정보처리기사 시나공 299

---
+++ src/main/java/test/ex/test299.java
@@ -0,0 +1,9 @@
+package test.ex;
+
+public class test299 {
+    public static void main(String[] args) {
+        int a =1, b =3, c=3, d =4;
+        if((a==b)&(c++ != d)&&(c++<6)) c++;
+        System.out.println(c);
+    }
+}
